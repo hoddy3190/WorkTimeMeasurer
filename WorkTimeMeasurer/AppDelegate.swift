5f6188855ac6e5e913916a86be880b44b2609387
Hodaka Suzuki <altitude3190@gmail.com>
2020-03-25 01:22:58 +0900

    Initial Commit

---
+++ WorkTimeMeasurer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WorkTimeMeasurer
+//
+//  Created by Hodaka Suzuki on 2020/03/25.
+//  Copyright © 2020 Hodaka Suzuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WorkTimeMeasurer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorkTimeMeasurer
+//
+//  Created by Hodaka Suzuki on 2020/03/25.
+//  Copyright © 2020 Hodaka Suzuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
