f96f4dcba4164625a408bfd0cc29daf404caf710
Hodaka Suzuki <altitude3190@gmail.com>
2020-03-25 01:51:43 +0900

    setting for creating a menu app
    4, 5 of https://qiita.com/Kyome/items/b85e7cfc2716a6f880ee#%E3%81%9D%E3%82%82%E3%81%9D%E3%82%82%E5%B8%B8%E9%A7%90%E5%9E%8B%E3%81%AE%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC%E3%83%90%E3%83%BC%E3%82%A2%E3%83%97%E3%83%AA%E3%81%A3%E3%81%A6%E3%81%A9%E3%81%86%E4%BD%9C%E3%82%8B%E3%81%AE%E3%82%88

--- WorkTimeMeasurer/AppDelegate.swift
+++ WorkTimeMeasurer/AppDelegate.swift
@@ -11,7 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    @IBOutlet weak var menu: NSMenu!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
