cf8e8c43cc703ffe96b8c426aae44aed255f92c3
Hodaka Suzuki <altitude3190@gmail.com>
2020-03-26 15:10:08 +0900

    ViewController of modal

--- WorkTimeMeasurer/ViewController.swift
+++ WorkTimeMeasurer/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            NSLog("hogehoge")
         // Do any additional setup after loading the view.
     }
 
