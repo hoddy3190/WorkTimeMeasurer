1a0b050471054fae33d59bf1caff6c5961ae7610
Hodaka Suzuki <altitude3190@gmail.com>
2020-03-27 00:08:38 +0900

    button event in TableColumn

--- WorkTimeMeasurer/ViewController.swift
+++ WorkTimeMeasurer/ViewController.swift
@@ -15,7 +15,7 @@ struct Record:Codable {
 }
 
 
-class ViewController: NSViewController, NSTableViewDataSource {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,10 +35,35 @@ class ViewController: NSViewController, NSTableViewDataSource {
         return programs.count
     }
     
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         tableView.headerView = nil
-        return programs[row]
+        
+        if (tableColumn == tableView.tableColumns[0]) {
+            NSLog("pppppppp")
+            let cellView = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "hogehogehoge"), owner: self) as! CustomCellView
+            //let buttonCell = cellView.imageView
+//            let cell = tableColumn?.dataCell(forRow: row)
+//            (cell as AnyObject).imageView?.image = NSImage(named: NSImage.actionTemplateName)
+            cellView.button.tag = row
+            return cellView
+        }
+        
+        
+//        return programs[row]
+        return nil
     }
+    
+    @IBAction func startTimer(_ sender: NSButtonCell) {
+        NSLog("fugafuga")
+        let hoge = sender.tag
 
+    }
 }
 
+// thx. https://www.appcoda.com/macos-programming-tableview/
+class CustomCellView: NSTableCellView {
+    
+    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var buttonCell: NSButtonCell!
+    
+}
